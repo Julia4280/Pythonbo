766614d04031bb3d8577855cbc7a91f6bb3a4f94
JuliaTorres <jtorres18791@iesjoanramis.org>
2022-10-27 11:50:06 +0200

    1r Commit en local

---
+++ ex2.py
@@ -0,0 +1,7 @@
+a = input("Introduaix un núumero: ")
+if (int(a)>18):
+    print(a, " és major de 18 anys")
+elif (int(a)<18):
+    print(a, "és menor de 18 anys")
+else:
+    print("Tens 18 anys!!")
